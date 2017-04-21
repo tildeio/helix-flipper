668028403b82baa61c3e1b24f6e333c461bfa444
Liz Baillie and Vaidehi Joshi <engineering+lb+vj@tilde.io>
2017-04-21 10:53:33 -0700

    Add Ruby methods to widen, narrow, and flip text

--- app/controllers/text_transform_controller.rb
+++ app/controllers/text_transform_controller.rb
@@ -2,8 +2,14 @@ class TextTransformController < ApplicationController
   def index
   end
 
-  def widen
-    transformed_text = TextTransform.new(params[:content]).transform
+  def transform
+    if params[:widen]
+      transformed_text = TextTransform.new(params[:content]).widen
+    elsif params[:narrow]
+      transformed_text = TextTransform.new(params[:content]).narrow
+    elsif params[:flip]
+      transformed_text = TextTransform.new(params[:content]).flip
+    end
 
     render json: transformed_text
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'text_transform#index'
-  post '/widen' => 'text_transform#widen'
+  post '/transform' => 'text_transform#transform'
 end
